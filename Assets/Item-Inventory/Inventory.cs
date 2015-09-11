150f5dd08f2e3ff64803fb81c7e1782f43cd5950
makiable <makiable@gmail.com>
2015-09-11 22:18:25 +0900

    간단한 인벤토리 만들기 했음.

---
+++ Assets/Item-Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour, IhasChanged{
+	[SerializeField] Transform slots;
+	[SerializeField] Text inventoryText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		HasChange ();
+	}
+
+	#region IhasChanged implementation
+	public void HasChange ()
+	{
+		System.Text.StringBuilder builder = new System.Text.StringBuilder ();
+		builder.Append (" = ");
+		foreach (Transform slotTransform in slots) {
+			GameObject item = slotTransform.GetComponent<Slot>().item;
+			if (item) {
+				builder.Append(item.name);
+				builder.Append(" = ");
+			}
+		}
+		inventoryText.text = builder.ToString ();
+	}
+	#endregion
+}
+
+namespace UnityEngine.EventSystems
+{
+	public interface IhasChanged:IEventSystemHandler{
+		void HasChange();
+	}
+
+}
